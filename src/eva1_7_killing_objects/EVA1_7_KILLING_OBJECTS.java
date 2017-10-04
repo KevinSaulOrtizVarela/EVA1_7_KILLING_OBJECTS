2837fee0811f5ccf44ca21dc05f90f89ba73cade
Kevin Ortiz <>
2017-10-03 22:30:41 -0600

    1.0

---
+++ src/eva1_7_killing_objects/EVA1_7_KILLING_OBJECTS.java
@@ -0,0 +1,17 @@
+package eva1_7_killing_objects;
+/*PROYECTO PARA DAR UN EJEMPLO DE COMO SE ELIMINA UN OBJETO*/
+/**
+ * @author 16550549
+ */
+public class EVA1_7_KILLING_OBJECTS {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Prueba pObj = new Prueba();
+    pObj= null; //ASI SE DESTRUYE UN OBJETO EN JAVA
+    
+    }
+    
+}
+class Prueba{
+    
+}
